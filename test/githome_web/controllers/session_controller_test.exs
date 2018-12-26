9ccaa921f4686a01585a4bbb15962540d4a0062c
roman <long399@mail.ru>
2018-12-26 15:29:42 +0700

    тест создания сессии

---
+++ test/githome_web/controllers/session_controller_test.exs
@@ -0,0 +1,21 @@
+defmodule GithomeWeb.SessionControllerTest do
+  use GithomeWeb.ConnCase
+
+  alias Githome.Users
+  alias String
+ import GithomeWeb.SessionController
+
+  @param %{token: "ITc3PC05Ek9nDBMCIGcpKhQBFz9yEAAAnQqeDvU8LKkar3ekV5tM+A==", username: "ololo"}
+
+
+
+  describe "Session" do
+    test "create new session", %{conn: conn} do
+	 new(conn, @param)
+	 |> Map.get(:status)
+     |> (fn (r) -> assert(302 == r) end).()
+	  
+    end
+  end 
+
+end
